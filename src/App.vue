d6c6a7552d05eb784fbcc5a411b8a1ed279a3490
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2019-10-29 01:21:00 +0800

    add monster animation

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,18 @@
   router-link(to="/")
     img(src='./assets/logo.png')
 
+  
+
   //- nav.navbar.navbar-light.bg-light
   //-   a.navbar-brand(href='#') Navbar
   // As a heading
+
+  .ball
+
+
+  .rect1
+    .rect2
+      .rect3
   nav.navbar.navbar-light.bg-dark
     span.navbar-brand.mb-0.h1 這是一條bootstrap的導覽列
     router-link(to="/HelloWorld") HelloWorld
@@ -53,6 +62,72 @@ export default {
 @import "~bootstrap/scss/bootstrap"
 
 #app
+  // .rect1
+  //   position: absolute
+  //   top: -31px
+  //   margin-left: 25px
+  //   border: 1px solid black
+  //   // background-color: green
+  //   width: 200px
+  //   height: 175px
+  //   transform: rotate(60deg)
+  //   // overflow: hidden
+
+  //   .rect2
+  //     border: 1px solid red
+  //     // background-color: white
+  //     top: 0px
+  //     width: 121px
+  //     height: 175px
+  //     margin-left: 100px
+  //     // transform: rotate(60deg)
+      
+
+
+  @keyframes ani
+    0%
+      left: 0
+      background-color: #35495E
+    25%
+      top: 10px
+    50%
+      border-radius: 50%
+      top: 100px
+    75%
+      top: 10px
+    100%
+      left: calc(100% - 100px)
+      background-color: #41B883
+      top: 100px
+
+  .ball
+    // border: 3px solid black
+    width: 100px
+    height: 100px
+    position: absolute
+    top: 100px
+    margin-left: 0px
+    
+    animation-name: ani
+    // 播放時間
+    animation-duration: 3s
+    //網頁進來後2秒開始
+    animation-delay: 2s
+    // 播放次數
+    animation-iteration-count: infinite
+    // 播放方向 alternate來回反彈
+    animation-direction: alternate
+    //往前後填補，抓0%影格的格式顏色
+    animation-fill-mode: both
+    
+    transition: 0.5s
+    &:hover
+      animation-play-state: paused
+      transform: rotate(45deg)
+      cursor: pointer
+  
+
+
   span
     color: white
 
