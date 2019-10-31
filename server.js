5be2ea8ee79e0ec74533fa98c6f8d264521fd988
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2019-10-31 23:27:10 +0800

    更新node

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+const serveStatic = require('serve-static')//靜態路徑
 const path = require('path')
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
-// const port = process.env.PORT || 80
-const port = process.env.PORT
+const port = process.env.PORT || 80
+// const port = process.env.PORT
 app.listen(port)
