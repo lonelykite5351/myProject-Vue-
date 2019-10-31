fde7495d8430e1736f2765eb561f37aa9291dfcd
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2019-11-01 00:18:10 +0800

    port空白

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require('serve-static')//靜態路徑
 const path = require('path')
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 8080
-// const port = process.env.PORT
+// const port = process.env.PORT || 8080
+const port = process.env.PORT
 app.listen(port)
